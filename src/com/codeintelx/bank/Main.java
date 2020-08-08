30c2b7b0d0939716a01ac4510235f9bebe15c6a5
Munirr Sesay <Munirr16@gmail.com>
2020-08-08 11:43:09 -0400

    First commit

---
+++ src/com/codeintelx/bank/Main.java
@@ -0,0 +1,24 @@
+package com.codeintelx.bank;
+
+import java.util.Scanner;
+
+public class Main {
+public static Scanner scanner = new Scanner();
+    public static void main(String[] args)
+    {
+
+    }
+    public static void printInstructions()
+    {
+        System.out.println("----------------------------------------Welcome!---------------------------------------- \n"
+        + "Please Select Options \n"
+        + "0. Display Options \n"
+        + "1. Create an Account \n"
+        + "2. View Account \n"
+        + "3. Withdraw from Account \n"
+        + "4. Deposit to Account \n");
+
+
+    }
+
+}
